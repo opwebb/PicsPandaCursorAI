fb85aac8be7e302bf2512e7677afb93387832316
Ajaym255 <ajaym255@users.noreply.github.com>
2025-03-18 08:08:26 +0530

    Add PWA support with service worker registration and offline page

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'picspanda-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/main.js',
+  '/images/picspanda-logo.svg',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
